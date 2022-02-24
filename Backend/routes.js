c366ef7d7f99e9738a786af5e922bec1a34877e0
Ali Osman <amrali975@gmail.com>
2022-02-25 00:37:52 +0200

    Added cors

--- Backend/routes.js
+++ Backend/routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 //------------------------------Internal ROUTES---------------------------\\
 const userRoutes = require("./API/Controllers/UserController");
 const messageRoutes = require("./API/Controllers/MessageController");
 //------------------------------Internal ROUTES---------------------------\\
 dotenv.config();
 const app = express();
+app.use(cors());
 app.use(
    express.json({
       limit: "5mb",
